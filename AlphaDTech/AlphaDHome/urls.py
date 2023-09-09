6fc0b9a0f82c6b65664c778c5f5036fb7ae48f51
MasonScarbro <masonscarbropf@gmail.com>
2023-09-09 15:56:23 -0400

    removed HTML and replaced with testing file

--- AlphaDTech/AlphaDHome/urls.py
+++ AlphaDTech/AlphaDHome/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, update_num
+from .views import home
 urlpatterns = [
      path('', home, name='home'),
-     path('update-num', update_num, name='update-num'),
 ]
--- AlphaDTech/AlphaDHome/views.py
+++ AlphaDTech/AlphaDHome/views.py
@@ -1,15 +1,46 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+import openai
+from decouple import config
+
+openai_api_key = config('OPENAI_API_KEY')
+openai.api_key = openai_api_key
+
+global response_string 
+
+# Potentially a new openAI instance that will handle the JSON from the AlphaFold API so that we can return a description of the protein
+
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            {"role": "system", "content": "You are a Helpful Assistant and Tutor"}, #Better Prompt this
+            {"role": "user", "content": message}
+        ]
+    )
+    answer = response.choices[0].message.content.strip()
+    return answer
 
 # Create your views here.
-num = 1
-def home(request):
-    global num
-    return render(request, 'home.html', {'num': num,})
 
-def update_num(request):
-    global num
+# Api function that returns the Json response here
+'''
+#Insert Implementation Here
+'''
+def home(request):
     if request.method == 'POST':
-        num = num + 1
-        response_data = {'num': num}
-        return JsonResponse(response_data)
+        message = request.POST.get('message')
+        response = ask_openai(message) #makes response the chatGPT response
+        
+        global response_string 
+        response_string = response
+        #Regex this based on prompt to only be the UkprotKit orr wtvr
+
+        return JsonResponse(
+            {
+            'message': message, 
+            'response': response,
+            #'AlphaResponse': AlphaResponse, 
+            })
+    return render(request, 'home.html')
